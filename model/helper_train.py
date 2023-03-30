0a07609d463e3decbb7e4c1d74ed40a477a783a3
anghol <angelkholicheva@gmail.com>
2023-03-30 11:20:21 +0000

    Created helper_train.py, added convolutional vae

---
+++ model/helper_train.py
@@ -0,0 +1,90 @@
+import time
+import torch
+import torch.nn as nn
+
+from model.vae import VAE
+
+
+def train(num_epochs: int, model: nn.Module, optimizer, device, train_loader, logging_interval: int, reconstruction_term_weight=1):
+    loss_fn = nn.functional.mse_loss
+
+    log_dict = {'train_combined_loss_per_batch': [],
+                'train_combined_loss_per_epoch': [],
+                'train_reconstruction_loss_per_batch': [],
+                'train_kl_loss_per_batch': []}
+
+    start_time = time.time()
+    for epoch in range(num_epochs):
+        model.train()
+
+        for batch_idx, features in enumerate(train_loader):
+            all_losses = training_step(features, model, device, optimizer,
+                                       loss_fn, reconstruction_term_weight)
+
+            logging(log_dict, all_losses, logging_interval,
+                    epoch, num_epochs, batch_idx, train_loader)
+    
+        print('Time elapsed: {:.2f} min'.format((time.time() - start_time) / 60))
+    
+    print('Total training time: {:.2f} min'.format((time.time() - start_time) / 60))
+
+    return log_dict
+
+
+def training_step(features, model, device, optimizer, loss_fn, reconstruction_term_weight):
+    features = features.to(device)
+    
+    # forward and backpropogation
+    encoded, z_mean, z_log_var, decoded = model(features)
+
+    # total loss = reconstruction loss + KL divergence
+    # kl_divergence = (0.5 * (z_mean**2 + torch.exp(z_log_var) - z_log_var - 1)).sum()
+
+    kl_div = -0.5 * torch.sum(
+        1 + z_log_var - z_mean**2 - torch.exp(z_log_var), axis=1
+    )  # sum over latent dimension  # type: ignore
+
+    batch_size = kl_div.size(0)
+    kl_div = kl_div.mean()  # average over batch dimension
+
+    pixelwise = loss_fn(decoded, features, reduction='none')
+    pixelwise = pixelwise.view(batch_size, -1).sum(axis=1)  # sum over pixels  # type: ignore
+    pixelwise = pixelwise.mean()  # average over batch dimension
+
+    loss = reconstruction_term_weight * pixelwise + kl_div
+
+    losses_dict = {'loss': loss.item(),
+                  'pixelwise': pixelwise.item(),
+                  'kl_div': kl_div.item()}
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    return losses_dict
+
+
+def logging(log_dict, losses_dict, logging_interval,
+            epoch, num_epochs, batch_idx, train_loader):
+    log_dict['train_combined_loss_per_batch'].append(losses_dict['loss'])
+    log_dict['train_reconstruction_loss_per_batch'].append(losses_dict['pixelwise'])
+    log_dict['train_kl_loss_per_batch'].append(losses_dict['kl_div'])
+
+    if not batch_idx % logging_interval:
+        print("Epoch: %03d/%03d | Batch %04d/%04d | Loss: %.4f" 
+              % (epoch + 1, num_epochs, batch_idx, len(train_loader), losses_dict['loss']))
+        
+
+def compute_epoch_loss_autoencoder(model, data_loader, loss_fn, device):
+    model.eval()
+    curr_loss, num_examples = 0., 0
+    with torch.no_grad():
+        for features in data_loader:
+            features = features.to(device)
+            decoded = model(features)[-1]
+            loss = loss_fn(decoded, features, reduction='sum')
+            num_examples += features.size(0)
+            curr_loss += loss
+
+        curr_loss = curr_loss / num_examples
+        return curr_loss
--- model/vae.py
+++ model/vae.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class Reshape(nn.Module):
     def __init__(self, *args):
         super().__init__()
@@ -19,9 +20,8 @@ class Trim(nn.Module):
         return x[:, :, :self.size, :self.size]
 
 
-# Remake this: create own classes for Decoder and Encoder
 class VAE(nn.Module):
-    def __init__(self, image_size: int, device: torch.device, latent_size=2):
+    def __init__(self, device: torch.device, image_size: int, latent_size=2):
         super().__init__()
         self.image_size = image_size
         self.latent_size = latent_size
@@ -65,11 +65,61 @@ class VAE(nn.Module):
         encoded = self.reparameterize(z_mean, z_log_var)
         decoded = self.decoder(encoded)
         return encoded, z_mean, z_log_var, decoded
+    
 
+class ConvolutionalVAE(nn.Module):
+    def __init__(self, device: torch.device, image_size: int, latent_size=2):
+        super().__init__()
+        self.image_size = image_size
+        self.latent_size = latent_size
+        self.device = device
 
-class Encoder(nn.Module):
-    pass
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 32, stride=(1, 1), kernel_size=(3, 3), padding=1),
+            nn.LeakyReLU(0.01),
+            nn.Conv2d(32, 64, stride=(2, 2), kernel_size=(3, 3), padding=1),
+            nn.LeakyReLU(0.01),
+            nn.Conv2d(64, 64, stride=(2, 2), kernel_size=(3, 3), padding=1),
+            nn.LeakyReLU(0.01),
+            nn.Conv2d(64, 64, stride=(2, 2), kernel_size=(3, 3), padding=1),
+            nn.LeakyReLU(0.01),
+            nn.Conv2d(64, 64, stride=(1, 1), kernel_size=(3, 3), padding=1),
+            nn.Flatten()
+        )
 
+        self.z_mean = torch.nn.Linear(3136, self.latent_size)
+        self.z_log_var = torch.nn.Linear(3136, self.latent_size)
 
-class Decoder(nn.Module):
-    pass
+        self.decoder = nn.Sequential(
+            torch.nn.Linear(self.latent_size, 3136),
+            Reshape(-1, 64, 7, 7),
+            nn.ConvTranspose2d(64, 64, stride=(1, 1), kernel_size=(3, 3), padding=1),
+            nn.LeakyReLU(0.01),
+            nn.ConvTranspose2d(64, 64, stride=(2, 2), kernel_size=(3, 3), padding=1),
+            nn.LeakyReLU(0.01),
+            nn.ConvTranspose2d(64, 64, stride=(2, 2), kernel_size=(3, 3), padding=0),
+            nn.LeakyReLU(0.01),
+            nn.ConvTranspose2d(64, 32, stride=(2, 2), kernel_size=(3, 3), padding=0),
+            nn.LeakyReLU(0.01),
+            nn.ConvTranspose2d(32, 1, stride=(1, 1), kernel_size=(3, 3), padding=0),
+            Trim(self.image_size),  # 1x57x57 -> 1x56x56
+            nn.Sigmoid()
+        )
+
+    def encoding_fn(self, x):
+        x = self.encoder(x)
+        z_mean, z_log_var = self.z_mean(x), self.z_log_var(x)
+        encoded = self.reparameterize(z_mean, z_log_var)
+        return encoded
+
+    def reparameterize(self, z_mu, z_log_var):
+        eps = torch.randn(z_mu.size(0), z_mu.size(1)).to(self.device)
+        z = z_mu + eps * torch.exp(z_log_var / 2.0)
+        return z
+
+    def forward(self, x):
+        x = self.encoder(x)
+        z_mean, z_log_var = self.z_mean(x), self.z_log_var(x)
+        encoded = self.reparameterize(z_mean, z_log_var)
+        decoded = self.decoder(encoded)
+        return encoded, z_mean, z_log_var, decoded
